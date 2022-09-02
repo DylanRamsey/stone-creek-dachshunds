72420ee8742d0bf45794a665c4a5c9b2fff07871
Dylan <dylanbradramsey@gmail.com>
2022-09-01 23:35:39 -0400

    Queens and testimonials home page components

--- front-page.php
+++ front-page.php
@@ -124,12 +124,12 @@
     </section>
     <section class="queens container">
         <h2 class="queens_h1">
-        <?php
-          $value = get_field("our_queens_heading");
-          if($value) {
-              echo $value;
-          }
-        ?>"  
+          <?php
+            $value = get_field("our_queens_heading");
+            if($value) {
+                echo $value;
+            }
+          ?>
         </h2>
       <section class="queen-boxes">
         <div class="queen-box">
@@ -139,7 +139,7 @@
               if($value) {
                   echo $value;
               }
-            ?>"
+            ?>
           </h3>
           <figure class="queen-box__image">
             <img src="
@@ -155,48 +155,139 @@
               if($value) {
                   echo $value;
               }
-            ?>">
+              ?>">
           </figure>
-          <button class="button button--light">MEET MILDRED</button>
+          <button class="button button--light">MEET 
+          <?php
+              $value = get_field("queen_one_name");
+              if($value) {
+                  echo $value;
+              }
+            ?>
+          </button>
         </div>
         <div class="queen-box">
           <h3 class="queen-box__heading">
-            Mildred
+          <?php
+              $value = get_field("queen_two_name");
+              if($value) {
+                  echo $value;
+              }
+          ?>
           </h3>
           <figure class="queen-box__image">
-            <img src="/assets/mildred.jpeg" alt="">
+            <img src="
+              <?php
+                $value = get_field("queen_two_image");
+                if($value) {
+                    echo $value;
+                }
+              ?>"
+            alt="
+            <?php
+              $value = get_field("queen_two_image");
+              if($value) {
+                  echo $value;
+              }
+              ?>">
           </figure>
-          <button class="button button--light">MEET DALIA</button>
+          <button class="button button--light">MEET 
+          <?php
+              $value = get_field("queen_two_name");
+              if($value) {
+                  echo $value;
+              }
+          ?>
+          </button>
         </div>
         <div class="queen-box">
           <h3 class="queen-box__heading">
-            Mildred
+          <?php
+              $value = get_field("queen_three_name");
+              if($value) {
+                  echo $value;
+              }
+          ?>
           </h3>
           <figure class="queen-box__image">
-            <img src="/assets/mildred.jpeg" alt="">
+            <img src="
+              <?php
+                $value = get_field("queen_three_image");
+                if($value) {
+                    echo $value;
+                }
+              ?>"
+            alt="
+            <?php
+              $value = get_field("queen_three_image");
+              if($value) {
+                  echo $value;
+              }
+              ?>">
           </figure>
-          <button class="button button--light">MEET AGNES</button>
+          <button class="button button--light">MEET 
+          <?php
+              $value = get_field("queen_three_name");
+              if($value) {
+                  echo $value;
+              }
+          ?>
+          </button>
         </div>
       </section>    
     </section>
     <section class="testimonial-banner">
       <figure class="testimonial-banner__image testimonial-banner__image--mobile">
-        <img src="/assets/testimonial-image.webp" alt="Girl holding dog">
+        <img src="
+        <?php
+            $value = get_field("home_testimonial_image");
+            if($value) {
+                echo $value;
+            }
+        ?>" 
+          alt="
+          <?php
+            $value = get_field("testimonial_image_alt_text");
+            if($value) {
+                echo $value;
+            }
+          ?>">
       </figure>
       <div class="testimonial-banner__container">
         <figure class="testimonial-banner__image testimonial-banner__image--desktop">
-          <img src="/assets/testimonial-image.webp" alt="Girl holding dog">
+          <img src="
+          <?php
+              $value = get_field("home_testimonial_image");
+              if($value) {
+                  echo $value;
+              }
+          ?>" 
+            alt="
+            <?php
+              $value = get_field("testimonial_image_alt_text");
+              if($value) {
+                  echo $value;
+              }
+            ?>">
         </figure>
         <div class="testimonial-banner__quote">
           <svg width="56px" height="56px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect x="0" fill="none" width="24" height="24"/><g><path d="M11.192 15.757c0-.88-.23-1.618-.69-2.217-.326-.412-.768-.683-1.327-.812-.55-.128-1.07-.137-1.54-.028-.16-.95.1-1.956.76-3.022.66-1.065 1.515-1.867 2.558-2.403L9.373 5c-.8.396-1.56.898-2.26 1.505-.71.607-1.34 1.305-1.9 2.094s-.98 1.68-1.25 2.69-.346 2.04-.217 3.1c.168 1.4.62 2.52 1.356 3.35.735.84 1.652 1.26 2.748 1.26.965 0 1.766-.29 2.4-.878.628-.576.94-1.365.94-2.368l.002.003zm9.124 0c0-.88-.23-1.618-.69-2.217-.326-.42-.77-.692-1.327-.817-.56-.124-1.074-.13-1.54-.022-.16-.94.09-1.95.75-3.02.66-1.06 1.514-1.86 2.557-2.4L18.49 5c-.8.396-1.555.898-2.26 1.505-.708.607-1.34 1.305-1.894 2.094-.556.79-.97 1.68-1.24 2.69-.273 1-.345 2.04-.217 3.1.165 1.4.615 2.52 1.35 3.35.732.833 1.646 1.25 2.742 1.25.967 0 1.768-.29 2.402-.876.627-.576.942-1.365.942-2.368v.01z"/></g></svg>
           <h5>
-            Stone Creek gave me my best friend. Gabie
-            set me up with the perfect puppy. Amazing
-            process during the adoption process, great
-            communication the entire time. Highly
-            recommended        
+            <?php
+              $value = get_field("home_testimonial");
+              if($value) {
+                  echo $value;
+              }
+            ?>    
           </h5>
-          <p>Dylan R</p>
+          <p>
+            <?php
+                $value = get_field("home_testimonial_author");
+                if($value) {
+                    echo $value;
+                }
+            ?>      
+          </p>
         </div>
     </div>
     </section>
@@ -205,7 +296,8 @@
     <h2 class="contact-us__heading h1">
       Contact Us
     </h2>
-    <p>contact form plugin goes here</p>
+    
+
   </section>
   <?php
     get_footer();
